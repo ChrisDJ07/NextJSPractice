2d13a749fcf28c97350b40118e43ca18692c3c94
Christian Dave Janiola <janiolaChrisJ07@gmail.com>
2025-02-19 20:54:50 +0800

    Progress - Chapter 16

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,3 +1,16 @@
-export default function Page() {
-    return <p>Customers Page</p>;
+import { Metadata } from 'next';
+import CustomersTable from '@/app/ui/customers/table';
+import { fetchFilteredCustomers } from '@/app/lib/data';
+
+export const metadata: Metadata = {
+    title: 'Customers',
+};
+
+
+export default async function Page() {
+    const customersList = await fetchFilteredCustomers('');
+
+    return <>
+        <CustomersTable customers={customersList} />
+    </>;
 }
